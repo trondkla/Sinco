eb47b2f8381bd97644b0dbf9109441508233e68d
trond klakken <trond.klakken@bekk.no>
2015-03-10 22:37:58 +0100

    force https

--- server.js
+++ server.js
@@ -8,6 +8,16 @@ app.use( bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 })); 
 
+function requireHTTPS(req, res, next) {
+    if (!req.secure) {
+        //FYI this should work for local development as well
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+
+app.use(requireHTTPS);
+
 app.set('port', process.env.PORT || 8888);
 
 app.post('/contact', function(req, res) {
